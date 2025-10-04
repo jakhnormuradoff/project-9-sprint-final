6d75823c27323a68c9e8ca9eee455767317b5aaa
Jakhongir Normurodov <jakhongir.normuradoff@gmail.com>
2025-10-04 19:29:51 +0500

    first commit

--- main_test.go
+++ main_test.go
@@ -1,3 +1,32 @@
 package main
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
 // Пишите тесты в этом файле
+func TestgenerateRandomElements(t *testing.T) {
+	// ваш код здесь
+	var size = []int{-1, 0, 100}
+
+	for _, v := range size {
+		result := generateRandomElements(v)
+		if v <= 0 {
+			assert.Equal(t, result, []int{})
+		}
+		assert.Equal(t, len(result), v)
+	}
+}
+
+func Testmaximum(t *testing.T) {
+	var data = []int{-1, 0, 100}
+	require.NotEqual(t, 0, len(data))
+
+	maxNum := maximum(data)
+	assert.Equal(t, 100, maxNum)
+}
+
+
